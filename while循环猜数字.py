e7a3e62745765ec3513f77d109dc4bd7af058d7a
dhbyx000c <40689629+dhbyx000c@users.noreply.github.com>
2020-08-23 21:31:35 +0800

    Add files via upload

---
+++ while循环猜数字.py
@@ -0,0 +1,29 @@
+# Author:FeiBuCat
+
+'''for i in range(1,10):
+    for j in range(1,i+1):
+        print('{}x{}={}\t'.format(j,i,i*j),end='')
+    print()
+'''
+
+Feibucat =30
+count =0
+while count <3:
+    guess = int(input("请输入数字： "))
+    if guess == Feibucat:
+        print("恭喜你答对了～！")
+        break
+    elif guess > Feibucat:
+        print("猜大了。。。")
+    elif guess < Feibucat:
+        print("猜小了。。。")
+    else:
+        print("猜错了，再来～！")
+    count +=1
+    if count ==3:
+        continue_confirm = input("游戏是否继续？")
+        if continue_confirm !='n':
+            count =0
+
+
+
